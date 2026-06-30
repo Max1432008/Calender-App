be7b9153770e0247ffbe53842dfac234d4830565
Max1432008 <Max.Oldehus@t-online.de>
2026-06-30 09:37:16 +0200

    base-structur

---
+++ frontend/script.js
@@ -0,0 +1,10 @@
+document.getElementById("Hallo").textContent = "Was geht ab";
+
+function getDaysInMonth(jahr, monat) {
+  const anzahlTage = new Date(jahr, monat, 0).getDate();
+  const tage = [];
+
+  // hier eine Schleife, die das Array füllt
+
+  return tage;
+}
